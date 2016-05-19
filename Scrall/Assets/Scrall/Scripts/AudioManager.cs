a1dc6535f2dc6f4937169b2f437641d56402fb39
prasadxna <prasadxna@hotmail.com>
2016-05-19 21:35:32 +0530

    Cardboard Check Completed
    For some reason Audio is not working in this project....

---
+++ Scrall/Assets/Scrall/Scripts/AudioManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+	AudioSource musicSource;
+	AudioSource sfxSource;
+
+	void playMusic(AudioClip clip)
+	{
+		musicSource.clip = clip;
+		musicSource.Play ();
+	}
+
+	void playSFX(AudioClip clip)
+	{
+		sfxSource.clip = clip;
+		sfxSource.Play ();
+	}
+
+	void playSFXWithVibration(AudioClip clip)
+	{
+		sfxSource.clip = clip;
+		Handheld.Vibrate ();
+		sfxSource.Play ();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Scrall/Assets/Scrall/Scripts/CheckCardboard.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class CheckCardboard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.deviceOrientation == DeviceOrientation.LandscapeLeft) {
+			SceneManager.LoadScene ("MainMenu");
+		}
+			
+	}
+
+	public void redirectToUrl(string url)
+	{
+		Application.OpenURL (url);
+	}
+}
